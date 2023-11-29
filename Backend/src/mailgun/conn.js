124cb5ae18492634e15d0367bacdb4f8a745ad8c
Lisandro <salvareschilisandro0@gmail.com>
2023-11-29 17:07:46 -0300

    mailgun configuration

--- Backend/src/mailgun/conn.js
+++ Backend/src/mailgun/conn.js
@@ -1,21 +1,10 @@
 const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-
-const mg = mailgun.client({
-	username: 'api',
-	key: '4f0067d7e9f24590dc1061d6843fc811',
+const Mailgun = require('mailgun-js')({
+	apiKey: process.env.MAILGUN_API_KEY,
+	domain: "sandbox3f37bcca0b8047a0ae1067367b17dde8.mailgun.org"
 });
 
-module.exports = mg;
+// const mailgun = new Mailgun(formData);
+// const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY })
 
-
-// mg.messages
-// 	.create(sandbox3f37bcca0b8047a0ae1067367b17dde8.mailgun.org, {
-// 		from: "Mailgun Sandbox <postmaster@sandbox3f37bcca0b8047a0ae1067367b17dde8.mailgun.org>",
-// 		to: ["salvareschilisandro0@gmail.com"],
-// 		subject: "Hello",
-// 		text: "Testing some Mailgun awesomness!",
-// 	})
-// 	.then(msg => console.log(msg)) // logs response data
-// 	.catch(err => console.log(err)); // logs any error`;
+module.exports = Mailgun;
