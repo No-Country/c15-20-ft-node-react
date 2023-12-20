5625f13c35275a400aa3d61ca66209a47d2791ad
[Pedro Sota] <psptaier@gmail.com>
2023-12-20 18:46:27 -0300

    cambio en about

--- Frontend/src/Pages/about/AboutPage.jsx
+++ Frontend/src/Pages/about/AboutPage.jsx
@@ -6,32 +6,35 @@ export default function AboutPage() {
     {
       name: "Johana Baccei",
       rol: "QA Analyst",
-      imageUrl: "https://media.licdn.com/dms/image/D4D03AQFOFDOIEkc4pA/profile-displayphoto-shrink_400_400/0/1678317652369?e=1708560000&v=beta&t=kqvVUjXlMA5nebzt2jYH937ORbnS3IrXfz98oVa1Ask",
-      linkedIn: "https://www.linkedin.com/in/johana-baccei-11b005248/"
+      imageUrl:
+        "https://media.licdn.com/dms/image/D4D03AQFOFDOIEkc4pA/profile-displayphoto-shrink_400_400/0/1678317652369?e=1708560000&v=beta&t=kqvVUjXlMA5nebzt2jYH937ORbnS3IrXfz98oVa1Ask",
+      linkedIn: "https://www.linkedin.com/in/johana-baccei-11b005248/",
     },
     {
       name: "Julián Cadavid",
       rol: "Project Manager",
-      imageUrl: "https://media.licdn.com/dms/image/D5603AQGOXAF0Ua3VHQ/profile-displayphoto-shrink_400_400/0/1701812598819?e=1708560000&v=beta&t=1rWYieINhFpOMR9p87baCK-wrbeHFxxV1Oz02cDbpD8",
-      linkedIn: "https://www.linkedin.com/in/juliancadavid07/"
+      imageUrl:
+        "https://media.licdn.com/dms/image/D5603AQGOXAF0Ua3VHQ/profile-displayphoto-shrink_400_400/0/1701812598819?e=1708560000&v=beta&t=1rWYieINhFpOMR9p87baCK-wrbeHFxxV1Oz02cDbpD8",
+      linkedIn: "https://www.linkedin.com/in/juliancadavid07/",
     },
     {
       name: "Arturo Guerra",
       rol: "Software Developer",
-      imageUrl: "https://media.licdn.com/dms/image/D4E03AQF6S-A7AXxLUA/profile-displayphoto-shrink_400_400/0/1698800025568?e=1708560000&v=beta&t=3IR5SQ98ntKPB7tSYc8BVHkaEGEdT3q30jz1lrv77tU",
-      linkedIn: "https://www.linkedin.com/in/arturo-dev/"
-    },    
+      imageUrl:
+        "https://media.licdn.com/dms/image/D4E03AQF6S-A7AXxLUA/profile-displayphoto-shrink_400_400/0/1698800025568?e=1708560000&v=beta&t=3IR5SQ98ntKPB7tSYc8BVHkaEGEdT3q30jz1lrv77tU",
+      linkedIn: "https://www.linkedin.com/in/arturo-dev/",
+    },
     {
       name: "Lisandro Salvarecci",
       rol: "Software Developer",
       imageUrl: "https://avatars.githubusercontent.com/u/47686025?v=4",
-      linkedIn: "https://www.linkedin.com/in/salvareschilisandro/"
+      linkedIn: "https://www.linkedin.com/in/salvareschilisandro/",
     },
     {
       name: "Pedro Sota",
       rol: "Software Developer",
       imageUrl: "https://avatars.githubusercontent.com/u/123599427?v=4",
-      linkedIn: "https://www.linkedin.com/in/pedroa-sota/"
+      linkedIn: "https://www.linkedin.com/in/pedroa-sota/",
     },
   ];
 
@@ -45,13 +48,31 @@ export default function AboutPage() {
           defaultIndex={0}
         >
           <TabList className='grid w-full grid-cols-4 gap-4'>
+            <Tab value='team'>Nuestro equipo</Tab>
             <Tab value='mission'>Nuestra misión</Tab>
             <Tab value='values'>Nuestros valores</Tab>
-            <Tab value='team'>Nuestro equipo</Tab>
             <Tab value='clients'>Nuestros clientes</Tab>
           </TabList>
 
           <TabPanels>
+            <TabPanel value='team'>
+              <section className='w-full py-12 md:py-24 lg:py-32'>
+                <div className='container px-4 md:px-6'>
+                  <div className='flex flex-col items-center space-y-4 text-center'>
+                    <div className='space-y-2'>
+                      <h1 className='text-3xl font-grotesk-font font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none'>
+                        Nuestro equipo
+                      </h1>
+                      <div className='flex flex-wrap justify-center gap-4 p-3'>
+                        {team.map((teammate) => (
+                          <OurTeam key={teammate.id} teammate={teammate} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </TabPanel>
             <TabPanel value='mission'>
               <section className='w-full py-12 md:py-24 lg:py-32'>
                 <div className='container px-4 md:px-6'>
@@ -98,22 +119,7 @@ export default function AboutPage() {
                 </div>
               </section>
             </TabPanel>
-            <TabPanel value='team'>
-              <section className='w-full py-12 md:py-24 lg:py-32'>
-                <div className='container px-4 md:px-6'>
-                  <div className='flex flex-col items-center space-y-4 text-center'>
-                    <div className='space-y-2'>
-                      <h1 className='text-3xl font-grotesk-font font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none'>
-                        Nuestro equipo
-                      </h1>
-                      <div className="flex flex-wrap justify-center gap-4 p-3">
-                        {team.map((teammate) => (<OurTeam key={teammate.id} teammate={teammate} />))}
-                        </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </TabPanel>
+
             <TabPanel value='clients'>
               <section className='w-full py-12 md:py-24 lg:py-32'>
                 <div className='container px-4 md:px-6'>
